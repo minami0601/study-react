72a61d94dd49056a5639a9707ce86e745762a776
minami0601 <minami.love.bb@icloud.com>
2021-04-19 20:57:05 +0900

    🏗 _appを用いた共通化

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,12 +3,12 @@ import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
-    </div>
+    </>
   );
 }
 
