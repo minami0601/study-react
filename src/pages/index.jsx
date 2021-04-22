c8d249da1774d98ea3957f65028ec700012cc53b
minami0601 <minami.love.bb@icloud.com>
2021-04-22 23:35:11 +0900

    ⚗ クリックイベント追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,28 @@
-import Head from 'next/head'
-import styles from 'src/styles/Home.module.css'
-import { Footer } from 'src/components/Footer'
-import { Links } from 'src/components/Links'
-import { Headline } from 'src/components/Headline'
-import { Main } from 'src/components/Main'
-import { Header } from 'src/components/Header'
+import Head from "next/head";
+import styles from "src/styles/Home.module.css";
+import { Footer } from "src/components/Footer";
+import { Links } from "src/components/Links";
+import { Headline } from "src/components/Headline";
+import { Main } from "src/components/Main";
+import { Header } from "src/components/Header";
+import { useCallback } from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Index Page</title>
-      </Head>
-      <Header />
-      <Main page="index" />
-      <Footer />
-    </div>
-  )
+	const handleClick = useCallback((e) => {
+		console.log(e.target.href);
+		e.preventDefault();
+	},[]);
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Index Page</title>
+			</Head>
+			<Header />
+			<a href="./about" onClick={handleClick}>
+				ボタン
+			</a>
+			<Main page="index" />
+			<Footer />
+		</div>
+	);
 }
