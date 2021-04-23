6c6edd44c57985e22e4889318e97f4813c4cd99a
minami0601 <minami.love.bb@icloud.com>
2021-04-23 22:07:40 +0900

    ♻️ リファクタリング

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,11 +7,11 @@ import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 import { useCallback } from "react";
 
+const handleClick = useCallback((e) => {
+	console.log(e.target.href);
+	e.preventDefault();
+},[]);
 export default function Home() {
-	const handleClick = useCallback((e) => {
-		console.log(e.target.href);
-		e.preventDefault();
-	},[]);
 	return (
 		<div className={styles.container}>
 			<Head>
